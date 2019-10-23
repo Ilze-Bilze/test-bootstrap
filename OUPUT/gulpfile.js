1e92cddd1dd96c348408b668c31efe459522c1d0
Ilze Spruge <ilzespruge@gmail.com>
2019-10-23 22:10:41 +0100

    adding bootstrap

--- OUPUT/gulpfile.js
+++ OUPUT/gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var sassPaths = [
+  'node_modules/bootstrap/scss'
+];
+
 // Compile 
 gulp.task('sass', function () { 
-	return gulp.src('app/scss/app.scss')
+	return gulp.src('app/scss/*.scss')
 	.pipe(sass({
-      outputStyle: 'compressed' // if css compressed **file size**
+		includePaths: sassPaths,
+      	outputStyle: 'compressed' // if css compressed **file size**
     })
 		.on('error', sass.logError))
 	.pipe(gulp.dest('app/css'));
 });
+
 // Compile and watch 
 gulp.task('watch', function () {
     return gulp.watch('app/scss/*.scss', gulp.series('sass'));
